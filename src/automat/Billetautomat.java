118f3954b2f17b05284a4535b92d0dfde65b0f1e
Big Fat Llama <lukas@10.16.99.208>
2015-10-05 15:37:51 +0200

    Ændret billeter solgt

--- src/automat/Billetautomat.java
+++ src/automat/Billetautomat.java
@@ -47,7 +47,7 @@ public class Billetautomat {
 	 * Opdater total og nedskriv balancen med billetprisen
 	 */
 	public void udskrivBillet() {
-		if (balance<10) {
+		if (balance<billetpris) {
 			System.out.println("Du mangler at indbetale nogle penge");
 		}
 		System.out.println("##########B##T#########");
