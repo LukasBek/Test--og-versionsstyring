93b80a3ce6e9c9a33766a50d581b9ea5936e87dd
Big Fat Llama <lukas@10.16.99.208>
2015-10-17 20:32:07 +0200

    Yderlige rettelse af billetsalg, når bruger ikke har penge til at købe billet

--- src/automat/Billetautomat.java
+++ src/automat/Billetautomat.java
@@ -49,7 +49,7 @@ public class Billetautomat {
 	public void udskrivBillet() {
 		if (balance<billetpris) {
 			System.out.println("Du mangler at indbetale nogle penge");
-		}
+		} else {
 		System.out.println("##########B##T#########");
 		System.out.println("# BlueJ Trafikselskab #");
 		System.out.println("#                     #");
@@ -63,6 +63,7 @@ public class Billetautomat {
 
 		antalBilletterSolgt = antalBilletterSolgt + 1;
 		balance = balance - billetpris; // Billetter koster 10 kroner
+		}
 	}
 
 
